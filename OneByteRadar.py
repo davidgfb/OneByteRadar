c989238dac2e777abed2f04211a90e605cb8aa70
davidgfb <davidgracia99@gmail.com>
2021-09-03 21:17:31 +0200

    Update OneByteRadar.py

--- OneByteRadar.py
+++ OneByteRadar.py
@@ -1,13 +1,17 @@
-import pymem
-import re
+from pymem         import Pymem
+from pymem.process import module_from_name
 
-pm = pymem.Pymem('csgo.exe')
-client = pymem.process.module_from_name(pm.process_handle,
-                                        'client.dll')
+from re import search
 
-clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08',
-                                         clientModule).start() + 6
+pm = Pymem('csgo.exe')
+client = module_from_name(pm.process_handle, 'client.dll')
 
-pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
+clientModule = pm.read_bytes(client.lpBaseOfDll,\
+                             client.SizeOfImage)
+address = search(rb'\x80\xB9.{5}\x74\x12\x8B\x41\x08',\
+                 clientModule)
+address = address.start()
+address = client.lpBaseOfDll + address + 6
+
+pm.write_uchar(address, 2)
 pm.close_process()
